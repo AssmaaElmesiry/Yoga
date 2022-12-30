6c06fddfdae73ec462c38358fa3297a3599487be
AssmaaElmesiry <assmaaelmesiry@gmail.com>
2022-12-30 23:24:25 +0200

    a

--- src/components/WhatWeDo.vue
+++ src/components/WhatWeDo.vue
@@ -71,7 +71,7 @@ export default {
     },
     setup() {
         
-        const imageUrl = new URL("../assets/", import.meta.url).href;
+        const imageUrl = new URL("../../src/assets/", import.meta.url).href;
         return { imageUrl };
     },
 }
