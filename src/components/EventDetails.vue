82558f76cb48f49679365d247bc02ebe7bde3108
AssmaaElmesiry <assmaaelmesiry@gmail.com>
2023-02-25 03:22:55 +0200

    add new section

--- src/components/EventDetails.vue
+++ src/components/EventDetails.vue
@@ -1,7 +1,14 @@
 <template>
     <section>
-        <div class="banner">
-            <img src="../assets/banner.jpg" />
+        <headerStyleVue />
+        <div class="banner w-full pt-28">
+            <!-- <img src="../assets/banner.jpg" /> -->
+            <h2>Event Details</h2>
+            <span>
+                <a href="#">Home</a> 
+
+                <a href="#">Event Details</a>
+            </span>
         </div>
         
     </section>
@@ -9,11 +16,23 @@
 </template>
 
 <script>
+import headerStyleVue from './headerStyle.vue'
 export default {
-
+    components:{
+        headerStyleVue,
+    }
 }
 </script>
 
-<style>
+<style lang="css">
+    .banner{
+        background-image: url("../../src/assets/banner.png");
+        background-size: cover;
+        width: 100%;
+        height: 500px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
 </style>
